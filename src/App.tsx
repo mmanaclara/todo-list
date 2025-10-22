3569a422204006ff05d46b15f19296e120e713f4
Clara Moreira <anacacamm@gmail.com>
2025-10-22 11:38:21 -0300

    feat: create badge component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Icon from "./components/Icon";
 
 import TrashIcon from "./assets/icons/trash.svg?react";
 import CheckIcon from "./assets/icons/check.svg?react";
+import Badge from "./components/Badge";
 
 export default function App() {
   return (
@@ -11,6 +12,10 @@ export default function App() {
         Hello World <Icon svg={TrashIcon} className="fill-pink-base" />
       </Text>
       <Icon svg={CheckIcon} className="w-8 fill-green-base" />
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge>2 de 5</Badge>
+      </div>
     </>
   );
 }
